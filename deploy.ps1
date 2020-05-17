e68caf979c5d1c72bf41c8d8efcd313fbd102e18
demikaiser <demikaiser13@gmail.com>
2020-05-17 03:02:41 -0700

    Updates contents (auto-generated from the deploy script).

--- deploy.ps1
+++ deploy.ps1
@@ -1,15 +1,9 @@
 
-
-
-# 
+# Execute the infoJson generation scripts.
 node ./scripts/makePathInfoJsonForBlogs.js
 node ./scripts/makePathInfoJsonForPortfolios.js
-
-
-
 
 # Publish to the GitHub Pages by Pushing it to the Master Branch.
 git add .
 git commit -m "Updates contents (auto-generated from the deploy script)."
 git push
-
